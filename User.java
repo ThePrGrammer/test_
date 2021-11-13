92943729ec3a8106e1100f134e8284e9173e1a10
Gcina <cebekhulug04@gmail.com>
2021-11-13 17:39:02 +0200

    modified User.java : added String name field

--- User.java
+++ User.java
@@ -2,11 +2,13 @@
 public class User {
 
     private String name;
+    private String surname;
     private int age;
 
-    public User(String name, int age){
+    public User(String name, String surname, int age){
 
         this.name = name;
+        this.surname = surname;
         this.age = age;
     }
 
